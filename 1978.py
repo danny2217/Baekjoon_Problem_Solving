82b324fe9b20e75effac0389a0d7ab4f5039ac66
dann2217 <danny2217@naver.com>
2026-01-21 07:24:48 +0900

    solve: Baekjoon no.1978

---
+++ 1978.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+nums = list(map(int, input().split()))
+cnt = 0
+for i in nums:
+    if i == 1: 
+        continue
+
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        cnt += 1
+print(cnt)
